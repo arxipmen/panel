08ceae93b0c0b7bdf308848a6724e91941e69601
Arkhipov Andrey <arxipovdev@gmail.com>
2016-05-17 15:20:41 +0300

    Создал модель "City" и миграцию

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517121127) do
+ActiveRecord::Schema.define(version: 20160517121715) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "region_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "countries", force: :cascade do |t|
     t.string   "name"
