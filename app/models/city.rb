c70699a7e16d4898e67f6865cff9caa31b6a7991
Arkhipov Andrey <arxipovdev@gmail.com>
2016-05-19 16:14:52 +0300

    Сделал CRUD для модели City

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,2 +1,7 @@
 class City < ActiveRecord::Base
+  validates :name,      presence: true
+  validates :region_id, numericality: true
+
+  belongs_to  :region
+
 end
--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,8 @@
 class Region < ActiveRecord::Base
+  validates :name,        presence: true
+  validates :code,        presence: true, numericality: true
+  validates :country_id,  presence: true, numericality: true
 
-  belongs_to :country
+  belongs_to  :country
+  has_many    :city, dependent: :destroy
 end
